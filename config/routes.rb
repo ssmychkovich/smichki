04ea865916a22692f69da8367d526986c1223bbd
Sergei Smychkovich <ssmychkovich@Sergeis-Mac-mini.local>
2017-01-04 21:09:23 -0500

    added home route for masquarade

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'photos/index'
 
   get 'welcome/index'
+  get 'home' => 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
